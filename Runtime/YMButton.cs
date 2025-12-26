9c8d19d8514ba424530fdcd2cc4657fc23f605c6
tsutsumi <tsutsumikiriku@gmail.com>
2025-12-26 23:46:47 +0900

    update: デバック処理をテストでコミット

--- Runtime/YMButton.cs
+++ Runtime/YMButton.cs
@@ -5,12 +5,11 @@ public class YMButton : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("YMButton Called");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
